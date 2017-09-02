9c572e8ec970a4aa5364f98dc10bd811648f0b2b
Thomas Zakrajsek <tzakrajs@linux.com>
2017-09-02 15:12:58 -0700

    the right way to get the path of the rc directory

--- setup.sh
+++ setup.sh
@@ -1,6 +1,11 @@
-path=$(echo $0|sed -e 's/setup.sh//')
+path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo $path
+home="/home/`whoami`"
+echo $home
 for i in `ls -a $path`; do
-  if [[ "$i" != "setup.sh" && "$i" != "." && "$i" != ".." ]]; then
-    ln -s $path$i ~/$i
+  if [[ "$i" != "setup.sh" && "$i" != "." && "$i" != ".." && "$i" != ".git" ]]; then
+    rm -f $home/$i.old
+    mv $home/$i $home/$i.old
+    ln -s $path/$i $home/$i
   fi
 done
